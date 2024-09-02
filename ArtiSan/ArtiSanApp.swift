170a7a70317a322663c9884697a2e988b6526917
Michael Steeber <michaelsteeber@me.com>
2024-09-02 18:18:41 -0500

    Initial Commit

---
+++ ArtiSan/ArtiSanApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtiSanApp.swift
+//  ArtiSan
+//
+//  Created by Michael Steeber on 9/2/24.
+//
+
+import SwiftUI
+
+@main
+struct ArtiSanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
